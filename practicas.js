c34acac1b0c7b5c162c87349be3fd979a57aac65
FabricioLaraignee <laraigneefabricio@hotmail.com>
2022-03-21 23:17:53 -0300

    primer commit

---
+++ practicas.js
@@ -0,0 +1,11 @@
+/* Números pares
+Hagamos una función más. La función debe llamarse esPar, debe recibir un número como parámetro,
+ y nos devolverá un booleano (true/false) indicándonos si ese número es divisible por dos.
+
+Tips: utilizar la consola para probar la función esPar y observar si devuelve el resultado esperado. 
+Recordar utilizar el operador módulo. */
+
+function esPar (n){
+return n % 2
+}
+console.log(esPar(8))
